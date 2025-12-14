51a52998b136085709522d81303568dac004f48b
Ashik Biju <ashikbiju1112@depaul.edu.in>
2025-12-14 21:56:54 +0530

    Fix SPA routing for Vercel

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -18,10 +18,10 @@ export default function Login() {
     await supabase.auth.signInWithOAuth({
   provider: "google",
   options: {
-    redirectTo: window.location.origin
-,
+    redirectTo: window.location.origin + "/#/auth",
   },
 });
+
 
   };
 
