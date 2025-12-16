b49e030b2c5f32099bf0ab99b93326737ec85273
Ashik Biju <ashikbiju1112@depaul.edu.in>
2025-12-16 19:32:05 +0530

    Updated feature X

---
+++ src/hooks/useAuth.ts
@@ -0,0 +1,21 @@
+// src/hooks/useAuth.ts
+import { useEffect, useState } from "react";
+import { supabase } from "../lib/supabase";
+import type { User } from "@supabase/supabase-js";
+
+export const useAuth = () => {
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const { data } = await supabase.auth.getUser();
+      setUser(data.user);
+      setLoading(false);
+    };
+
+    fetchUser();
+  }, []);
+
+  return { user, loading };
+};
