143b4c17feaa689f82fa2dd1cd54ad1b1135b17e
Ashik Biju <ashikbiju1112@depaul.edu.in>
2025-12-16 19:22:15 +0530

    Updated feature X

--- src/components/ChatLayout.jsx
+++ src/components/ChatLayout.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { supabase } from "../lib/supabase";
 import "./chat.css";
 import UserSearch from "./UserSearch";
-//import { useAuth } from "../hooks/useAuth";
+import { useAuth } from "../hooks/useAuth";
 
 
 
